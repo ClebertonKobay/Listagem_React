c61968874b06c7ef90c0f42c77e4defeb983a131
Cleberton Kobay <Clebertonkobay@gmail.com>
2024-02-22 22:41:44 -0300

    search function

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,66 @@
-import { Plus, Search, FileDown, MoreHorizontal } from 'lucide-react';
+import { Plus, Search, FileDown, Filter, MoreHorizontal } from 'lucide-react';
 import { Header } from './components/header';
 import { Tabs } from './components/tabs';
 import { Button } from './components/ui/button';
 import { Control, Input } from './components/ui/input';
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from './components/ui/table';
 import { Pagination } from './components/pagination';
+import { useQuery, keepPreviousData } from '@tanstack/react-query';
+import { useSearchParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import useDebounceValue from './hooks/use_debounce_value';
+
+export interface TagResponse {
+  first: number
+  prev: number | null
+  next: number
+  last: number
+  pages: number
+  items: number
+  data: Tag[]
+}
+
+export interface Tag {
+  title: string
+  amountOfVideos: number
+  id: string
+}
+
+
+
 export function App() {
+  const [searchParams, setSearchParams] = useSearchParams()
+  const page = searchParams.get('page') ?? 1;
+  const urLFilter = searchParams.get('filter') ?? '';
+
+  const [filter, setFilter] = useState(urLFilter);
+
+
+  const debounceFilter = useDebounceValue(filter, 1000)
+
+  const { data: tagsResponse, isLoading } = useQuery<TagResponse>({
+    queryFn: async () => {
+      const response = await fetch(`http://localhost:3000/tags?_page=${page}&_per_page=10&title=${urLFilter}`)
+      const data = response.json();
+      console.log(data)
+      return data
+    },
+    queryKey: ['get-tags', urLFilter, page],
+    placeholderData: keepPreviousData
+  })
+
+  function handleFilter() {
+    setSearchParams(params => {
+      params.set('page', '1')
+      params.set('filter', filter)
+      return params
+    })
+  }
+
+
+  if (isLoading) {
+    return null
+  }
 
   return (
     <>
@@ -21,13 +76,21 @@ export function App() {
               <Plus className='size-3' />Create new
             </Button>
           </div>
-          <div className='flex items-center justify-between'>
-            <Input variant='filter'>
-              <Search className='size-3' />
-              <Control placeholder='Search tags...' />
-            </Input>
+          <form className='flex items-center justify-between'>
+            <div className='flex items-center '>
+              <Input variant='filter'>
+                <Search className='size-3' />
+                <Control placeholder='Search tags...'
+                  onChange={e => setFilter(e.target.value)}
+                  value={filter}
+                />
+              </Input>
+              <Button onClick={handleFilter}>
+                <Filter className='size-3' />
+              </Button>
+            </div>
             <Button><FileDown className='size-3' />Export</Button>
-          </div>
+          </form>
           <Table>
             <TableHeader>
               <TableRow>
@@ -38,16 +101,17 @@ export function App() {
               </TableRow>
             </TableHeader>
             <TableBody>
-              {Array.from({ length: 10 }).map((value, index) => {
+              {tagsResponse?.data.map((tag) => {
                 return (
-                  <TableRow key={index}>
+                  <TableRow key={tag.id}>
+                    <TableCell></TableCell>
                     <TableCell>
                       <div className='flex flex-col'>
-                        <span className='font-medium'>React</span>
-                        <span className='text-xs text-zinc-500'>oasndiuasnodiasmniudasoidniasn</span>
+                        <span className='font-medium'>{tag.title}</span>
+                        <span className='text-xs text-zinc-500'>{tag.id}</span>
                       </div>
                     </TableCell>
-                    <TableCell className='zinc-300'>13 Videos</TableCell>
+                    <TableCell className='zinc-300'>{tag.amountOfVideos} Videos</TableCell>
                     <TableCell className='text-right'>
                       <Button size='icon'><MoreHorizontal className='size-4' />
                       </Button>
@@ -57,7 +121,8 @@ export function App() {
               })}
             </TableBody>
           </Table>
-          <Pagination />
+          {tagsResponse && <Pagination pages={tagsResponse.pages} items={tagsResponse.items} page={Number(page)} />}
+
         </main>
       </div >
     </>
